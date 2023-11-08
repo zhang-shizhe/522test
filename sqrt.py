668af62a0c028349a2112109cb6320fc9aa2b767
shizhe <shizhe_zhang@outlook.com>
2023-11-08 12:14:58 -0800

    add expection

--- sqrt.py
+++ sqrt.py
@@ -16,7 +16,8 @@ opt = docopt(__doc__)
 
 def main(number):
     number = int(number)
-    print(math.sqrt(number))
+    if number < 0:
+        raise Exception("n should not a positive number")
     return math.sqrt(number)
 
 
